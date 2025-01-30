2886e5a0831e09258cdc4e8ac22e43b15fac705b
pawan <thakunipawan@gmail.com>
2025-01-30 18:22:08 +0530

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { WebSocketServer } from "ws";
-
-const wss=new WebSocketServer({port:8000})
+const wss = new WebSocketServer({ port: 8000 });
 
 //connection= it make connection ,whenever a connection made give me the socket of that pwrson,a place through which i can send msg and get msg
 wss.on("connection",(socket)=>{
 
 
-    wss.
+    socket.on("message", (e)=>{
+        if(e.toString() ==="ping"){
+            socket.send("pong")
+        }
+    })
 
 })
