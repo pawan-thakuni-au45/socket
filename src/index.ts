a48f09e4568fc8d50e5165527bfcc3e7a3d7273b
pawan <thakunipawan@gmail.com>
2025-01-30 17:55:57 +0530

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("ji")
+import { WebSocketServer } from "ws";
+
+const wss=new WebSocketServer({port:8000})
+
+//connection= it make connection ,whenever a connection made give me the socket of that pwrson,a place through which i can send msg and get msg
+wss.on("connection",(socket)=>{
+
+
+    wss.
+
+})
