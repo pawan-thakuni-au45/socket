3995dd8ae493f3806a0568b271945f2f07e0efa8
pawan <thakunipawan@gmail.com>
2025-02-01 09:33:26 +0530

    commit

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8000 });
-//connection= it make connection ,whenever a connection made give me the socket of that pwrson,a place through which i can send msg and get msg
 wss.on("connection", (socket) => {
-    socket.on("message", (e) => {
-        if (e.toString() === "ping") {
-            socket.send("pong");
-        }
+    wss.on("message", (message) => {
+        console.log(typeof (message));
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import { WebSocketServer } from "ws";
-const wss = new WebSocketServer({ port: 8000 });
+const wss=new WebSocketServer({port:8000})
 
-//connection= it make connection ,whenever a connection made give me the socket of that pwrson,a place through which i can send msg and get msg
+const User{
+    socket,
+    room
+}
+
+let allSocket:User[]=[]
+
 wss.on("connection",(socket)=>{
 
-
-    socket.on("message", (e)=>{
-        if(e.toString() ==="ping"){
-            socket.send("pong")
+    wss.on("message",(message)=>{
+        const parseMessage=JSON.parse(message)
+        if(parseMessage.type=="join"){
+           allSocket.push({
+            socket,
+            room:parseMessage.payload.roomId
+           })
         }
-    })
+
+    } )
 
 })
